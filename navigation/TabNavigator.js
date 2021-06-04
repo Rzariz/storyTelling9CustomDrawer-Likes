799d87180c2b25fec95830f9f3048ca0254e9228
Varsha <varshavasanthan366@gmail.com>
2021-06-04 18:13:22 +0530

    material UI & dropDownPicker

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -4,22 +4,28 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feed from "../screens/Feed";
 import CreateStory from "../screens/CreateStory";
 
-const Tab = createBottomTabNavigator();
+import { StyleSheet} from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { RFValue } from "react-native-responsive-fontsize";
+
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
         <Tab.Navigator
+            labeled={false}
+            barStyle={styles.bottomTabStyle}
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
                     if (route.name === 'Feed') {
                         iconName = focused
-                            ? 'book'
-                            : 'book-outline';
+                            ? 'home'
+                            : 'home-outline';
                     } else if (route.name === 'CreateStory') {
-                        iconName = focused ? 'create' : 'create-outline';
+                        iconName = focused ? 'add-circle' : 'add-circle-outline';
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />;
+                    return <Ionicons name={iconName} size={ RFValue(25) } color={color} style = {styles.icon} />;
                 },
             })}
             tabBarOptions={{
@@ -32,5 +38,21 @@ const BottomTabNavigator = () => {
         </Tab.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    bottomTabStyle: {
+      backgroundColor: "#2f345d",
+      height: "8%",
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      overflow: "hidden",
+      position: "absolute"
+    },
+    icons: {
+      width: RFValue(30),
+      height: RFValue(30)
+    }
+  });
+
 
 export default BottomTabNavigator
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,17 +1,106 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image
+} from 'react-native';
+import { RFValue } from "react-native-responsive-fontsize";
+import {FlatList} from 'react-native-gesture-handler'
+import StoryCard from './StoryCard'
 
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = { 'Bubblegum-Sans': require("../assets/fonts/BubblegumSans-Regular.ttf") }
+let stories = require("./temp_stories.json");
 export default class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        }
+    }
+    async loadFontAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true })
+    }
+
+    componentDidMount() {
+        this.loadFontAsync();
+    }
+    renderItem = ({ item : mystory}) =>{
+        return <StoryCard story = {mystory}/>
+    }
+    keyExtractor = (item, index) => index.toString();
     render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Feeds</Text>
-            </View>
-        )
+
+        if (!this.state.fontsLoaded) {
+            return <AppLoading />
+        }
+        else {
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <View style={styles.appTitle}>
+                        <View style={styles.appIcon}>
+                            <Image
+                                source={require("../assets/logo.png")} style={styles.iconImage}></Image>
+                        </View>
+                        <View style={styles.appTitleTextContainer}>
+                            <Text style={styles.appTitleText}> Storytelling App </Text>
+                        </View>
+                    </View>
+                    <View style = {styles.cardContainer}>
+                        <FlatList
+                        data = {stories}
+                        keyExtractor = {this.keyExtractor}
+                        renderItem = { this.renderItem}
+                        />
+                    </View>
+
+                </View>
+            )
+        }
+
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#15193c"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.7,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28),
+        fontFamily: "Bubblegum-Sans"
+    },
+    cardContainer: {
+        flex: 0.93
+    }
+})
